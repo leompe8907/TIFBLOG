8aee2d671482380b00b888fda56d408362e19830
leompe8907 <veyqpasa1312@gmail.com>
2024-07-08 14:52:01 -0300

    modificacion de la funcion para desloguear y verificar el token

--- static/logout.js
+++ static/logout.js
@@ -19,5 +19,15 @@ document.getElementById('logoutBtn').addEventListener('click', function() {
             alert(data.error || 'Error desconocido al cerrar sesión');
         }
     })
-    .catch(error => console.error('Error:', error));
+    .catch(error => {
+        console.error('Error:', error);
+        // Comprobar si el error es debido a un token expirado
+        if (error.message && (error.message.includes('token expired') || error.message.includes('Token inválido o expirado'))) {
+            localStorage.removeItem('token');
+            localStorage.removeItem('isLoggedIn');
+            window.location.href = 'index.html';
+        } else {
+            alert('Error desconocido al cerrar sesión');
+        }
+    });
 });
